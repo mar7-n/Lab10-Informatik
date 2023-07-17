98aafd25199600294f835670be5d2a46741b4ab7
phuchoang-devn <120661351+phuchoang-devn@users.noreply.github.com>
2023-07-17 15:33:48 +0200

    add index and home to detailView.py

--- studybuddy_site/studybuddy_app/views/detailView.py
+++ studybuddy_site/studybuddy_app/views/detailView.py
@@ -1,7 +1,14 @@
 from django.views.generic import DetailView
 from .models import Subject
+from django.shortcuts import render
 
 class SubjectDetailView(DetailView):
     model = Subject
     template_name = 'subject_detail.html'
     context_object_name = 'subject'
+
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
